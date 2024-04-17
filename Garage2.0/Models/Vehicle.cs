1f7e32a007fec43a21f6cd2f180521c3a0a2c026
Yogaledare <marcus_uppstrom@hotmail.com>
2024-04-17 11:33:52 +0200

    Vehicle.cs v1

---
+++ Garage2.0/Models/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage2._0.Models;
+
+public class Vehicle {
+    
+    [Key]
+    public string LicensePlate { get; set; } = string.Empty;
+    
+    public VehicleType? VehicleType { get; set; }
+
+    public string Color { get; set; } = string.Empty;
+
+    public string Brand { get; set; } = string.Empty;
+
+    public string Model { get; set; } = string.Empty;
+
+    public int NumberOfWheels { get; set; }
+
+    public DateTime ArrivalTime { get; set; }
+}
---
+++ Garage2.0/Models/VehicleType.cs
@@ -0,0 +1,12 @@
+﻿namespace Garage2._0.Models;
+
+public enum VehicleType {
+    
+    
+    Car, 
+    Truck, 
+    Bus,
+    Boat, 
+    
+    
+}
