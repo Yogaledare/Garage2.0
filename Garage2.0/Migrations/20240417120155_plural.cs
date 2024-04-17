8b309a73f21be71da86f19daca2eb01e834ec881
Yogaledare <marcus_uppstrom@hotmail.com>
2024-04-17 14:12:26 +0200

    plural

---
+++ Garage2.0/Migrations/20240417120155_plural.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Garage2._0.Migrations
+{
+    /// <inheritdoc />
+    public partial class plural : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vehicle",
+                table: "Vehicle");
+
+            migrationBuilder.RenameTable(
+                name: "Vehicle",
+                newName: "Vehicles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vehicles",
+                table: "Vehicles",
+                column: "LicensePlate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vehicles",
+                table: "Vehicles");
+
+            migrationBuilder.RenameTable(
+                name: "Vehicles",
+                newName: "Vehicle");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vehicle",
+                table: "Vehicle",
+                column: "LicensePlate");
+        }
+    }
+}
