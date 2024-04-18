bc6be03a55595aedcab06728a49b33a6cbb42e62
fionaliu66 <fiona.liu0606@gmail.com>
2024-04-18 13:03:37 +0200

    Invoice complete

--- Garage2.0/Models/InvoiceViewModel.cs
+++ Garage2.0/Models/InvoiceViewModel.cs
@@ -5,11 +5,16 @@
         public  string License;
         public DateTime ArriveD;
         public DateTime LeaveD;
+        public double TotalHours;
+        public int Price;
         public InvoiceViewModel(string licenseP, DateTime arriveD)
         {
             License = licenseP;
             ArriveD = arriveD;
             LeaveD = DateTime.Now;
+            TotalHours = (LeaveD - arriveD).TotalHours;
+            Price = (int)Math.Round(TotalHours)*15;
         }
     }
 }
+    
