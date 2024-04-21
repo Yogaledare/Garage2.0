4d2398c09998073e6a0d32d7aa5e41c33f457c78
Yogaledare <marcus_uppstrom@hotmail.com>
2024-04-22 01:14:00 +0200

    refactoring - moving parsing around

--- Garage2.0/wwwroot/js/charts.js
+++ Garage2.0/wwwroot/js/charts.js
@@ -22,30 +22,29 @@ const createDoughnutChart = (containerId, titleText, dataPoints) => {
 }
 
 
-const initializeChartsFromModel = (model) => {
-    const vehicleTypesData = model.vehicleTypeSummaries
-        .map(m => ({
-                label: m.vehicleType.toString(),
-                y: m.count
-            })
-        );
-    const wheelsData = model.vehicleTypeSummaries
-        .map(m => ({
-                label: m.vehicleType.toString(),
-                y: m.totalWheels
-            })
-        );
-
-    setupCharts(vehicleTypesData, wheelsData);
-}
-
-
-
-
-
 const setupCharts = (vehicleTypesData, wheelsData) => {
     createDoughnutChart("chartContainer", "Parked Vehicle Types", vehicleTypesData);
     createDoughnutChart("wheelsChartContainer", "Wheels Distribution", wheelsData);
 }
 
 
+//
+//
+// const initializeChartsFromModel = (model) => {
+//     const vehicleTypesData = model.vehicleTypeSummaries
+//         .map(m => ({
+//                 label: m.vehicleType.toString(),
+//                 y: m.count
+//             })
+//         );
+//     const wheelsData = model.vehicleTypeSummaries
+//         .map(m => ({
+//                 label: m.vehicleType.toString(),
+//                 y: m.totalWheels
+//             })
+//         );
+//
+//     setupCharts(vehicleTypesData, wheelsData);
+// }
+//
+//
