e02c0c3af614878f226630031a9038decdf81eb5
fionaliu66 <fiona.liu0606@gmail.com>
2024-04-23 11:31:12 +0200

    Add migration

---
+++ Garage2.0/Migrations/20240423081905_ParkingSpots.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Garage2._0.Migrations
+{
+    /// <inheritdoc />
+    public partial class ParkingSpots : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_ParkingSpot_ParkingSpotId",
+                table: "Vehicles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ParkingSpot",
+                table: "ParkingSpot");
+
+            migrationBuilder.RenameTable(
+                name: "ParkingSpot",
+                newName: "ParkingSpots");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ParkingSpots",
+                table: "ParkingSpots",
+                column: "ParkingSpotId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_ParkingSpots_ParkingSpotId",
+                table: "Vehicles",
+                column: "ParkingSpotId",
+                principalTable: "ParkingSpots",
+                principalColumn: "ParkingSpotId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_ParkingSpots_ParkingSpotId",
+                table: "Vehicles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ParkingSpots",
+                table: "ParkingSpots");
+
+            migrationBuilder.RenameTable(
+                name: "ParkingSpots",
+                newName: "ParkingSpot");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ParkingSpot",
+                table: "ParkingSpot",
+                column: "ParkingSpotId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_ParkingSpot_ParkingSpotId",
+                table: "Vehicles",
+                column: "ParkingSpotId",
+                principalTable: "ParkingSpot",
+                principalColumn: "ParkingSpotId");
+        }
+    }
+}
